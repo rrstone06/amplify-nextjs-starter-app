5d2594cce978fcdc0f96a17891e3690dfc109e5d
rrstone06 <60783222+rrstone06@users.noreply.github.com>
2024-02-13 08:29:53 -0500

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -15,22 +15,22 @@ export const auth = defineAuth({
        // then, import `secret` from `@aws-amplify/backend`
        // @see https://docs.amplify.aws/gen2/deploy-and-host/sandbox-environments/features/#setting-secrets
        
-      loginWithAmazon: {
-        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
+      //loginWithAmazon: {
+        //clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+        //clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
       }
     },
-  },
+  ,
   /**
    * enable multifactor authentication
    * @see https://docs.amplify.aws/gen2/build-a-backend/auth/manage-mfa
    */
-  multifactor: {
-    mode: 'OPTIONAL',
-    sms: {
-      smsMessage: (code) => `Your verification code is ${code}`,
-    },
-  },
+  //multifactor: {
+  //  mode: 'OPTIONAL',
+  //  sms: {
+  //    smsMessage: (code) => `Your verification code is ${code}`,
+  //  },
+ // },
   userAttributes: {
     /** request additional attributes for your app's users */
     // profilePicture: {
